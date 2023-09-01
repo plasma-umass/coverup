4a50825cbe7d7c41d248fdf3b67f11f32153ccd9
Juan Altmayer Pizzorno <juan@altmayer.com>
2023-09-01 11:44:14 -0400

    - refactored code a bit;
    - fixed missing handling for when the target file isn't even in the
      coverage results;

--- coverup.py
+++ coverup.py
@@ -5,6 +5,7 @@ import os
 import ast
 from collections import defaultdict
 from pathlib import Path
+import typing
 
 
 PREFIX = 'coverup'
@@ -60,7 +61,43 @@ def delete_last_test():
         pass
 
 
-def get_missing_coverage(jsonfile, base_path = ''):
+class CodeSegment:
+    def __init__(self, filename: Path, name: str, begin: int, end: int, missing_lines: typing.Set[int],
+                 context: typing.List[typing.Tuple[int, int]]):
+        self.filename = filename
+        self.name = name
+        self.begin = begin
+        self.end = end
+        self.missing_lines = missing_lines
+        self.context = context
+
+
+    def __str__(self):
+        return f"CodeSegment({self.name}, \"{self.filename}\" {self.begin}-{self.end-1})"
+
+
+    def get_excerpt(self, base_path = ''):
+        excerpt = []
+        with open(base_path + self.filename, "r") as src:
+            code = src.readlines()
+
+            for b, e in self.context:
+                for i in range(b, e):
+                    excerpt.extend([f"{i:10}: ", code[i-1]])
+
+            for i in range(self.begin, self.end):
+                excerpt.extend([f"{i:10}: ", code[i-1]])
+
+        return ''.join(excerpt)
+
+
+    def get_missing(self):
+        # FIXME compress by using ranges
+        return ", ".join(map(str, self.missing_lines))
+
+
+# FIXME make size_limit configurable; base it on tokens, not lines?
+def get_missing_coverage(jsonfile, base_path = '', size_limit=100):
     """Processes a JSON SlipCover output and generates a list of Python code segments,
     such as functions or classes, which have less than 100% coverage.
     """
@@ -83,7 +120,7 @@ def get_missing_coverage(jsonfile, base_path = ''):
         for d in node.decorator_list: # skip back to include decorators, in case they matter
             begin = min(begin, d.lineno)
 
-        return (begin, node.end_lineno)
+        return (begin, node.end_lineno+1)   # +1 for range() style
 
     for fname in cov['files']:
         with open(base_path + fname, "r") as src:
@@ -98,8 +135,7 @@ def get_missing_coverage(jsonfile, base_path = ''):
 
                 context = []
 
-                # FIXME make configurable; base it on tokens, not lines
-                while end - begin > 100:
+                while end - begin > size_limit:
                     child = find_enclosing(node, line)
                     if not child:
                         break
@@ -114,9 +150,9 @@ def get_missing_coverage(jsonfile, base_path = ''):
 
         if code_this_file:
             for it in code_this_file:
-                name, first, last = it
+                name, begin, end = it
                 missing = code_this_file[it]
-                code_segs.append([fname, name, (first, last), list(missing), ctx_this_file[it]])
+                code_segs.append(CodeSegment(fname, name, begin, end, missing, ctx_this_file[it]))
 
     return code_segs
 
@@ -140,36 +176,25 @@ def measure_coverage(test: str):
     return cov["files"]
 
 
-def improve_coverage(missing, base_path = ''):
-    fname, objname, line_range, missing_lines, context = missing
-    print(f"\n=== {objname} ({fname}) ===")
+def improve_coverage(seg: CodeSegment):
+    print(f"\n=== {seg.name} ({seg.filename}) ===")
 
-    excerpt = []
-    with open(base_path + fname, "r") as src:
-        code = src.readlines()
-
-        for b, e in context:
-            for i in range(b, e):
-                excerpt.extend([f"{i:10}: ", code[i-1]])
-
-        b, e = line_range
-        for i in range(b, e+1):
-            excerpt.extend([f"{i:10}: ", code[i-1]])
-
-    excerpt = ''.join(excerpt)
+    def pl(item, singular, plural = None):
+        if len(item) <= 1:
+            return singular
+        return plural if plural is not None else f"{singular}s"
 
     messages = [{"role": "user",
                  "content": f"""
-The code below, extracted from {fname}, does not achieve full line coverage:
-when tested, {'lines' if len(missing_lines)>1 else 'line'} {", ".join(map(str, missing_lines))}
-{'do' if len(missing_lines)>1 else 'does'} not execute.
+The code below, extracted from {seg.filename}, does not achieve full line coverage:
+when tested, {pl(seg.missing_lines,'line')} {seg.get_missing()} {pl(seg.missing_lines,'does','do')} not execute.
 Create a new pytest test function that executes these missing lines, always checking
 using the provided function that the new version is correct and indeed improves
 coverage. Do not propose a new version without first checking that it is correct
 and that it provides improved coverage. Always send entire Python test scripts
 when proposing a new test or correcting one you previously proposed.
 ```python
-{excerpt}
+{seg.get_excerpt()}
 ```
 """
             }]
@@ -201,7 +226,8 @@ when proposing a new test or correcting one you previously proposed.
                 break
 
             except (openai.error.ServiceUnavailableError,
-                    openai.error.RateLimitError) as e:
+                    openai.error.RateLimitError,
+                    openai.error.Timeout) as e:
                 print(e)
                 print(f"waiting {sleep}s)")
                 import time
@@ -236,25 +262,25 @@ when proposing a new test or correcting one you previously proposed.
 
         try:
             result = measure_coverage(last_test)
-            result = result[fname]
 
-            orig_missing = set(missing_lines)
-            new_covered = set(result['executed_lines'])
-            now_missing = orig_missing - new_covered
+            new_covered = set(result[seg.filename]['executed_lines']) if seg.filename in result else set()
+            now_missing = seg.missing_lines - new_covered
 
-            print(f"Previously missing: {list(orig_missing)}")
+            print(f"Originally missing: {list(seg.missing_lines)}")
             print(f"Still missing:      {list(now_missing)}")
 
-            if len(now_missing) < len(orig_missing):
+            # XXX insist on len(now_missing) == 0 while saving best test?
+            if len(now_missing) < len(seg.missing_lines):
                 # good 'nough
+                # FIXME change this to save_test and writing to temporary files while trying
                 get_test_path(advance=True)
                 break
 
             messages.append({
                 "role": "user",
                 "content": f"""
-This test does not improve coverage: {'lines' if len(missing_lines)>1 else 'line'}
-{", ".join(map(str, missing_lines))} still {'do' if len(missing_lines)>1 else 'does'} not execute.
+This test still lacks coverage: {'lines' if len(now_missing)>1 else 'line'}
+{", ".join(map(str, now_missing))} still {'do' if len(now_missing)>1 else 'does'} not execute.
 """
             })
             log.write(messages[-1]['content'] + "\n---\n")
@@ -273,13 +299,12 @@ This test does not improve coverage: {'lines' if len(missing_lines)>1 else 'line
             log.write(messages[-1]['content'] + "\n---\n")
 
 
-for m in get_missing_coverage(args.cov_json):
-    fname, objname, line_range, missing_lines, context = m
-    if not fname.startswith(args.source_dir):
+for seg in get_missing_coverage(args.cov_json):
+    if not seg.filename.startswith(args.source_dir):
         continue
 
-    if args.source_file and args.source_file not in fname:
-        print(f"skipping {m}")
+    if args.source_file and args.source_file not in seg.filename:
+        print(f"skipping {seg}")
         continue
 
-    improve_coverage(m)
+    improve_coverage(seg)
