88ac82d3af8e20c6e226ec308940c4aefd3e28da
Juan Altmayer Pizzorno <juan@altmayer.com>
2024-04-15 10:45:30 -0400

    - deleting unused code;

--- src/coverup/utils.py
+++ src/coverup/utils.py
@@ -3,26 +3,6 @@ import typing as T
 import subprocess
 
 
-class TemporaryOverwrite:
-    """Context handler that overwrites a file, and restores it upon exit."""
-    def __init__(self, file: Path, new_content: str):
-        self.file = file
-        self.new_content = new_content
-        self.backup = file.parent / (file.name + ".bak") if file.exists() else None
-
-    def __enter__(self):
-        if self.file.exists():
-            self.file.replace(self.backup)
-
-        self.file.write_text(self.new_content)
-        self.file.touch()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.file.unlink()
-        if self.backup:
-            self.backup.replace(self.file)
-
-
 def format_ranges(lines: T.Set[int], negative: T.Set[int]) -> str:
     """Formats sets of line numbers as comma-separated lists, collapsing neighboring lines into ranges
        for brevity."""
